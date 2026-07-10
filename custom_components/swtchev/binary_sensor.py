8b0749d262f35aea7f813c100cca6963ce659509
arthvpatel <67969520+arthvpatel@users.noreply.github.com>
2026-07-10 13:03:39 -0400

    Remove EntityCategory and display prefs from swtchev
    Remove EntityCategory usage and related entity metadata from swtchev sensor and binary_sensor definitions. This removes entity_category attributes, several suggested_display_precision/suggested_unit_of_measurement and entity_registry visibility defaults, and drops the 'PLUG' device_class on the connected binary sensor. Also change meter_raw to use float and remove the integer-rounding logic in the sensor entity. Files changed: custom_components/swtchev/binary_sensor.py, custom_components/swtchev/sensor.py.

--- custom_components/swtchev/binary_sensor.py
+++ custom_components/swtchev/binary_sensor.py
@@ -7,7 +7,6 @@ from dataclasses import dataclass
 from homeassistant.components.binary_sensor import BinarySensorDeviceClass, BinarySensorEntity, BinarySensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
@@ -28,7 +27,6 @@ BINARY_SENSORS: tuple[SwtchBinarySensorDescription, ...] = (
         key="online",
         name="Online",
         path=("data", "csInfo", "isOnline"),
-        entity_category=EntityCategory.DIAGNOSTIC,
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
         value_type="bool",
     ),
@@ -36,13 +34,11 @@ BINARY_SENSORS: tuple[SwtchBinarySensorDescription, ...] = (
         key="occupied",
         name="Occupied",
         path=("data", "csInfo", "isOccupied"),
-        entity_category=EntityCategory.DIAGNOSTIC,
         value_type="bool",
     ),
     SwtchBinarySensorDescription(
         key="connected",
         name="Connected",
-        device_class=BinarySensorDeviceClass.PLUG,
         value_type="connected",
     ),
     SwtchBinarySensorDescription(
